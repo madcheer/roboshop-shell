04b9d5a03107a028e5e41aa3e27152f6287ff494
madan <madanmohan.devops@gmail.com>
2023-12-22 18:47:37 +0530

    roboshop-shell

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+AMI=ami-03265a0778a880afb
+SG_ID=sg-0c40ab29368c1d034
+
+INSTANCES=('mongodb" "catalogue" "user")
+
+for [ i in $INSTANCES ]
+do
+   if [ $i == 'mongodb']
+     then 
+        INSTANCE_TYPE="t3.small"
+     else
+        INSTANCE_TYPE="t2.micro"
+
+    fi
+aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0c40ab29368c1d034
+ 
+done
+
